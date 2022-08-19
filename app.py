c139d99730026ef61a296bb610d4349b12497ea8
klovaaxel <klovakarlsson@gmail.com>
2022-08-19 18:28:24 +0200

    refactor(py): Basic rendertemplate setup

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import webbrowser
 
 app = Flask(__name__)
 @app.route('/')
-def hello_world():
-    return 'Hello world!'
+def schedule():
+    return render_template('schedule.html', id = id)
 
 if __name__ == '__main__':
     webbrowser.open('http://127.0.0.1:5000/')
