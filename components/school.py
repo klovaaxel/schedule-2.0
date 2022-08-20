33d955f6337d71606dbe43f10c7bab9490d7a293
klovaaxel <klovakarlsson@gmail.com>
2022-08-21 00:20:56 +0200

    refactor(py):More built out object for groups

--- components/school.py
+++ components/school.py
@@ -1,61 +1,96 @@
 schoolWeeks = [33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
 
 
-groups = [
-    {
-    'id' : 3,
-    'name': '20tei',
-        'courses': [
-            {
-                'name': 'Webutveckling 2',
-                'id': 'weuweb02',
-                'weekplan': {
-                    33:'Kolla på gitarr jäveln',
-                    35:'Crasha docker'
-                },
-                'assignments': [
-                    {
-                        'name' : 'Drupal',
-                        'brief' : 'En stor och ful sida',
-                    }
-                ]
+groups = {
+    '20tei' : {
+        'id' : 3,
+        'name': '20tei',
+        'courses': {
+                'weuweb02' : {
+                    'name': 'Webutveckling 2',
+                    'weekplan': {
+                        33:'Kolla på gitarr jäveln',
+                        34:'',
+                        35:'Crasha docker',
+                        36:'',
+                        37:'',
+                        38:'',
+                        39:'',
+                        40:'',
+                        41:'',
+                        42:'',
+                        43:'',
 
+                    },
+                    'assignments': [
+                        {
+                            'name' : 'Drupal',
+                            'brief' : 'En stor och ful sida',
+                            'start-week': 33,
+                            'end-week': 34,
+                        },
+                        {
+                            'name' : 'Drupal',
+                            'brief' : 'En stor och ful sida',
+                            'start-week': 33,
+                            'end-week': 34,
+                        },
+                    ]
+
+                },
+                'nainar' : {
+                    'name': 'Nätverksteknik 1&2',
+                    'weekplan': {
+                        33:'Uppstart uppkopplade slipsar och gräsmattor',
+                    },
+                    'assignments': [
+                        {
+                            'name' : 'PaketTracer',
+                            'brief' : 'Ping Ping Ping ...',
+                            'start-week': '',
+                            'end-week': '',
+                        }
+                    ]
+
+                }
             },
-            {
-                'name': 'Nätverksteknik 1&2',
-                'id': 'nainar',
-                'weekplan': {
-                    33:'Uppstart uppkopplade slipsar och gräsmattor',
-                },
-                'assignments': [
-                    {
-                        'name' : 'PaketTracer',
-                        'brief' : 'Ping Ping Ping ...',
-                    }
-                ]
+        },
+        '21tei' : {
+        'id' : 2,
+        'name': '21tei',
+        'courses': {
+                'weuweb01' : {
+                    'name': 'Webutveckling 1',
+                    'weekplan': {
+                        33:'Kolla på gitarr jäveln',
+                        34:'',
+                        35:'Crasha docker',
+                        36:'',
+                        37:'',
+                        38:'',
+                        39:'',
+                        40:'',
+                        41:'',
+                        42:'',
+                        43:'',
 
-            }
-        ],
-    },
-    {
-    'id': 2,
-    'name': '21tei',
-        'courses': [
-            {
-                'name': 'Webutveckling 1',
-                'id': 'weuweb01',
-                'weekplan': {
-                    33:'Kolla på HTMLDOG',
-                    35:'Sätta Hotpink på allt'
-                },
-                'assignments': [
-                    {
-                        'name' : 'Min Fula Lilla Sida',
-                        'brief' : 'En liten och ful sida',
-                    }
-                ]
+                    },
+                    'assignments': [
+                        {
+                            'name' : 'Min fulla lilla sida',
+                            'brief' : 'En liten och ful sida',
+                            'start-week': 33,
+                            'end-week': 34,
+                        },
+                        {
+                            'name' : 'Projektet',
+                            'brief' : 'Grupparbete för fan',
+                            'start-week': 33,
+                            'end-week': 34,
+                        },
+                    ]
 
-            }
-        ],
-    },
-]
+                },
+            },
+        },
+    }
